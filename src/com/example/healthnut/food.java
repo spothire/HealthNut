d64d7e8e4d2c63ac02e912a1b39914623ee48fbf
spothire <spothire@andrew.cmu.edu>
2014-04-15 12:59:09 -0400

    food class

---
+++ src/com/example/healthnut/food.java
@@ -0,0 +1,28 @@
+package com.example.healthnut;
+
+public class food {
+
+	 int food_id;
+	 String food_name;
+	 int food_calories;
+	 
+	public int getFood_id() {
+		return food_id;
+	}
+	public void setFood_id(int food_id) {
+		this.food_id = food_id;
+	}
+	public String getFood_name() {
+		return food_name;
+	}
+	public void setFood_name(String food_name) {
+		this.food_name = food_name;
+	}
+	public int getFood_calories() {
+		return food_calories;
+	}
+	public void setFood_calories(int food_calories) {
+		this.food_calories = food_calories;
+	}
+	
+}
